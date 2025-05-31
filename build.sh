8d2e98e444043fa88152b4ec1ad4b84a30f6c499
cysof <cp4reallife@yahoo.com>
2025-05-31 10:31:53 +0100

    build file

--- build.sh
+++ build.sh
@@ -5,10 +5,10 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 
-if [[ $CREATE_SUPERUSER ]];
-then
-  python manage.py createsuperuser --no-input
-fi
+# if [[ $CREATE_SUPERUSER ]];
+# then
+#   python manage.py createsuperuser --no-input
+# fi
 
 
 python manage.py migrate
